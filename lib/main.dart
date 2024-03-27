47b557a4080d4e3b746ffc07266c8a03fc9e0d89
Asiri Pramodaya <asiripramodaya2000@gmail.com>
2024-03-27 18:29:29 +0530

    customized text fields and button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -10,41 +11,78 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Column(
             children: [
-              Text("Log In",style: TextStyle(color: Colors.black, fontSize:45 ),),
-              SizedBox(
+              const Text("Log In",style: TextStyle(color: Colors.black, fontSize:45 ),),
+              const SizedBox(
                 height: 100,
               ),
               Padding(
-                padding: EdgeInsets.all(20.0),
+                padding: EdgeInsets.all(50.0),
                 child: TextField(
                   decoration: InputDecoration(
+                    fillColor: Colors.blue[100],
                     hintText: "Email",
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(color: Colors.blue)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(color: Color.fromARGB(255, 0, 140, 255))
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(color: Colors.red)
+                    ),
                   ),
                 ),
               ),
-              SizedBox(
-                height: 75,
+              const SizedBox(
+                height: 10,
               ),
               Padding(
-                padding: EdgeInsets.all(20.0),
+                padding: EdgeInsets.all(50.0),
                 child: TextField(
                   decoration: InputDecoration(
                     hintText: "Password",
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(color: Colors.blue)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(color: Color.fromARGB(255, 0, 140, 255))
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(color: Colors.red)
+                    ),
                   ),
                 ),
               ),
-              SizedBox(
-                height: 75,
+              const SizedBox(
+                height: 20,
               ),
-              ElevatedButton(
-                onPressed: (){}, 
-                child: Text("Login"))             
+              Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: SizedBox(
+                  width:double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: (){},
+                    child: Text("Login"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      
+                    ),
+                    ),
+                ),
+              )             
             ],
           ),
           
